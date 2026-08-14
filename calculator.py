330b9791dfcdac78560512d7e24f96dadecab31c
mitali-pawar <mitalipawar486@gmail.com>
2026-08-15 00:57:29 +0530

    Implement basic calculator for addition and multiplication

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+multiplication = num1 * num2
+
+print("Addition:", addition)
+print("Multiplication:", multiplication)
